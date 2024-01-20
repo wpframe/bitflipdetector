1d152cb2182294d6e5747ba098494ecb4c1a0361
Will Frame <will@wpf.nz>
2024-01-21 12:40:38 +1300

    Create bitflipdetector.c

---
+++ bitflipdetector.c
@@ -0,0 +1,39 @@
+#include <signal.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#include <unistd.h>
+
+volatile int interrupted = 0;
+
+void interrupt_handler(int sig){
+    if(sig == SIGINT) interrupted = 1;
+}
+
+int main(){
+    int tests = 0;
+    int flips = 0;
+    int size = 1073741824;
+    unsigned char *bytes = (unsigned char*)calloc(size, 1);
+    memset(bytes, 0, size);
+    time_t t = time(NULL);
+    printf("Program started at unix time %ld\n", t);
+    signal(SIGINT, interrupt_handler);
+    while(!interrupted){
+        for(int i = 0; i < size; i++){
+            flips += bytes[i];
+            if(flips > 0){
+                printf("Bit flip detected!\n");
+                printf("Unix time: %ld\n", t);
+                printf("Test number: %d\n", tests);
+                printf("Position in byte array: %d/1073741824\n", i);
+            }
+        }
+        tests++;
+        flips = 0;
+        sleep(1);
+    }
+    free(bytes);
+    return 0;
+}
